40c3d6bcfbfdcb3a3ba93f9d822e7f79ac6c4641
coffeeologist <liuamy000@gmail.com>
2020-05-16 20:04:57 -0400

    trying react-only tutorial

---
+++ app/src/containers/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../img/icon.png';
+
+function Nav () {
+    return(
+      <div className="nav">
+        <img className="App-logo" src={logo} alt="logo"/>
+      </div>
+    )
+}
+
+export default Nav;
